05ecba6c57773cf1b78179557b1b9fcfffbe45eb
JirkaDellOro <del@hs-furtwangen.de>
2025-10-06 19:21:46 +0200

    Quiz first version works

--- Quiz/Quiz.js
+++ Quiz/Quiz.js
@@ -1,35 +1,46 @@
 var Quiz;
 (function (Quiz) {
+    var ƒ = FudgeCore;
     let TASK;
     (function (TASK) {
         TASK[TASK["SC"] = 0] = "SC";
         TASK[TASK["MC"] = 1] = "MC";
     })(TASK || (TASK = {}));
     const tasks = [
-        { type: TASK.SC, question: "Which city is germany's capital?", answers: ["Berlin", "Hamburg", "München", "Frankfurt"], correct: 1 }
+        { type: TASK.SC, question: "Which city is germany's capital?", options: ["Berlin", "Hamburg", "München", "Frankfurt"], correct: 0 }
     ];
     window.addEventListener("load", start);
+    const task = tasks[0];
     function start() {
-        createHTML(tasks[0]);
+        createHTML(task);
+        document.querySelector("button").addEventListener("click", () => validate(task));
+    }
+    function validate(_task) {
+        const formdata = new FormData(document.forms[0]);
+        const result = formdata.get("option");
+        console.log(result == "" + _task.correct);
     }
     function createHTML(_task) {
         let question = document.querySelector("div#Question");
         let options = document.querySelector("div#Options");
         question.innerText = tasks[0].question;
-        for (let answer in _task.answers) {
-            console.log(answer);
-            let button = document.createElement("input");
-            button.type = "radio";
-            button.id = "answer" + answer;
-            button.name = "answers";
-            let label = document.createElement("label");
-            label.htmlFor = button.id;
-            label.innerText = _task.answers[answer];
-            let option = document.createElement("span");
-            options.appendChild(option);
-            option.appendChild(button);
-            option.appendChild(label);
+        const spans = [];
+        for (let option in _task.options) {
+            const input = document.createElement("input");
+            input.type = "radio";
+            input.id = "option" + option;
+            input.name = "option";
+            input.value = option;
+            const label = document.createElement("label");
+            label.htmlFor = input.id;
+            label.innerText = _task.options[option];
+            const span = document.createElement("span");
+            span.appendChild(input);
+            span.appendChild(label);
+            spans.push(span);
         }
+        while (spans.length)
+            options.appendChild(spans.splice(ƒ.random.getIndex(spans), 1)[0]);
     }
 })(Quiz || (Quiz = {}));
 //# sourceMappingURL=Quiz.js.map
--- Quiz/Quiz.ts
+++ Quiz/Quiz.ts
@@ -1,39 +1,53 @@
 namespace Quiz {
+  import ƒ = FudgeCore;
+
   enum TASK {
     SC, MC
   }
-  type Task = { type: TASK, question: string, answers: string[], correct: number | number[] };
+  type Task = { type: TASK, question: string, options: string[], correct: number | number[] };
 
   const tasks: Task[] = [
-    { type: TASK.SC, question: "Which city is germany's capital?", answers: ["Berlin", "Hamburg", "München", "Frankfurt"], correct: 1 }
+    { type: TASK.SC, question: "Which city is germany's capital?", options: ["Berlin", "Hamburg", "München", "Frankfurt"], correct: 0 }
   ];
 
   window.addEventListener("load", start);
+  const task: Task = tasks[0];
 
   function start(): void {
-    createHTML(tasks[0]);
+    createHTML(task);
+    document.querySelector("button")!.addEventListener("click", () => validate(task));
   }
 
-  function createHTML(_task: Task) {
+  function validate(_task: Task): void {
+    const formdata: FormData = new FormData(document.forms[0]);
+    const result: string = <string>formdata.get("option");
+    console.log(result == "" + _task.correct);
+  }
 
+  function createHTML(_task: Task): void {
     let question: HTMLDivElement = document.querySelector("div#Question")!;
     let options: HTMLDivElement = document.querySelector("div#Options")!;
     question.innerText = tasks[0].question;
+    const spans: HTMLSpanElement[] = [];
 
-    for (let answer in _task.answers) {
-      console.log(answer);
-      let button: HTMLInputElement = document.createElement("input");
-      button.type = "radio";
-      button.id = "answer" + answer;
-      button.name = "answers";
-      let label: HTMLLabelElement = document.createElement("label");
-      label.htmlFor = button.id;
-      label.innerText = _task.answers[answer];
-      
-      let option: HTMLSpanElement = document.createElement("span");
-      options.appendChild(option);
-      option.appendChild(button);
-      option.appendChild(label);
+    for (let option in _task.options) {
+      const input: HTMLInputElement = document.createElement("input");
+      input.type = "radio";
+      input.id = "option" + option;
+      input.name = "option";
+      input.value = option;
+
+      const label: HTMLLabelElement = document.createElement("label");
+      label.htmlFor = input.id;
+      label.innerText = _task.options[option];
+
+      const span: HTMLSpanElement = document.createElement("span");
+      span.appendChild(input);
+      span.appendChild(label);
+      spans.push(span);
     }
+
+    while (spans.length)
+      options.appendChild(spans.splice(ƒ.random.getIndex(spans), 1)[0]);
   }
 }
